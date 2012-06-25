0eca5e546afe70123b781eaa04e7eb7d8c0aa9e6
Scott Rabin <scottrabin@gmail.com>
2012-06-24 20:39:45 -0700

    Added cache ability to url fetching

--- lib/showrobot.rb
+++ lib/showrobot.rb
@@ -1,7 +1,29 @@
 module ShowRobot
 
+	require 'pathname'
+	require 'fileutils'
+
+	# ShowRobot constants
+
+	# Determines if ShowRobot will attempt to use the query cache
+	USE_CACHE = true unless defined?(::ShowRobot::USE_CACHE)
+	# Determines the directory ShowRobot will use for the query cache
+	if ::ShowRobot::USE_CACHE
+		if not defined?(::ShowRobot::CACHE_DIRECTORY)
+			CACHE_DIRECTORY = [File.dirname(__FILE__) + '/..', ENV['HOME'], '.'].find { |path| File.writable? path } + '/.showrobot_cache' rescue nil
+		end
+
+		if CACHE_DIRECTORY.nil?
+			$stderr.puts "Cannot write to any default directory for ShowRobot cache"
+		else
+			FileUtils.mkdir_p CACHE_DIRECTORY
+		end
+	end
+
 	class << self
+		# verbosity of output
 		attr_accessor :verbose
+
 	end
 
 end
--- lib/showrobot/utility/fetch.rb
+++ lib/showrobot/utility/fetch.rb
@@ -9,11 +9,29 @@ module ShowRobot
 	def fetch type, url
 		case type
 		when :xml
-			XML::Parser.string(open(url).read).parse
+			XML::Parser.string(fetch_cache(url)).parse
 		else
 			raise "Invalid datatype to fetch: [ #{type.to_s} ]"
 		end
 	end
-	module_function :fetch
+
+	def fetch_cache url
+		cache_file = File.join(ShowRobot::CACHE_DIRECTORY, url.gsub(/([^a-zA-Z0-9_\.-]+)/) { '%' + $1.unpack('H2' * $1.size).join('%').upcase }.tr(' ', '+'))
+		in_cache = ShowRobot::USE_CACHE && File.exists?(cache_file)
+
+		if in_cache
+			puts "Found cache entry for [ #{url} ]" if ShowRobot.verbose
+			File.read cache_file
+		else
+			contents = open(url).read
+
+			if ShowRobot::USE_CACHE
+				puts "Created cache entry for [ #{url} ] at [ #{cache_file} ]" if ShowRobot.verbose
+				File.open(cache_file, 'w') { |f| f.write(contents) }
+			end
+			contents
+		end
+	end
+	module_function :fetch, :fetch_cache
 
 end

386eef97530a91799156614b5a133773751f2bdc
Scott Rabin <scottrabin@gmail.com>
2012-07-01 16:08:11 -0700

    Added specification for movie list sorting

--- spec/movie_datasource_spec.rb
+++ spec/movie_datasource_spec.rb
@@ -5,14 +5,14 @@ require 'text'
 describe ShowRobot, 'movie datasource API' do
 
 	before :each do
-		@file = ShowRobot::MediaFile.load 'First Movie(2002).avi'
+		@file = ShowRobot::MediaFile.load 'First Movie.avi'
 		@db   = ShowRobot.create_datasource :mockmovie
 		@db.mediaFile = @file
 	end
 
 	describe 'when querying for a list of matches' do
 		it 'should return a list of movies' do
-			@db.movie_list.should have(8).items
+			@db.movie_list.should have(12).items
 			@db.movie_list.each do |movie|
 				movie[:title].should be_an_instance_of(String)
 				movie[:year].should be_a_kind_of(Integer)
@@ -22,12 +22,65 @@ describe ShowRobot, 'movie datasource API' do
 	end
 
 	describe 'when prioritizing the returned list' do
-		describe 'when there is no runtime data' do
+		describe 'when there is no runtime or year data' do
 			it 'should order them by word distance to title' do
 				last_distance = 0
 				@db.movie_list.each do |movie|
 					distance = Text::Levenshtein.distance(movie[:title], @file.name_guess)
 					last_distance.should <= distance
+					last_distance = distance
+				end
+			end
+		end
+
+		describe 'when there is year data but no runtime data' do
+			it 'should filter the movie list by matching year' do
+				@file.year = 2002
+				@db.movie_list.should have(2).items
+				@db.movie_list.each do |movie|
+					movie[:year].should eq(2002)
+				end
+			end
+		end
+
+		describe 'when there is runtime data but no year data' do
+			it 'should order the movie list by word distance to title them by closest runtime' do
+				@file.runtime = 65
+				@db.movie_list.should have(14).items
+				last_distance, last_runtime_diff = 0, 0
+				@db.movie_list.each do |movie|
+					distance = Text::Levenshtein.distance(movie[:title], @file.name_guess)
+					last_distance.should <= distance
+					if last_distance == distance
+						last_runtime_diff.should <= (movie[:runtime] - @file.runtime)
+						last_runtime_diff = movie[:runtime] - @file.runtime
+					else
+						last_runtime_diff = 0
+					end
+					last_distance = distance
+				end
+			end
+		end
+
+		describe 'when there is runtime and year data' do
+			it 'should filter the list by year and sort by word distance then by closest runtime' do
+				@file.year = 2003
+				@file.runtime = 87
+				# verify filtering
+				@db.movie_list.should have(2).items
+				last_distance, last_runtime_diff = 0, 0
+				@db.movie_list.each do |movie|
+					# verify filtering, part 2
+					movie[:year].should eq(2003)
+					# verify distance
+					distance = Text::Levenshtein.distance(movie[:title], @file.name_guess)
+					last_distance.should <= distance
+					if last_distance == distance
+						last_runtime_diff.should <= (movie[:runtime] - @file.runtime)
+						last_runtime_diff = movie[:runtime] - @file.runtime
+					else
+						last_runtime_diff = 0
+					end
 					last_distance = distance
 				end
 			end

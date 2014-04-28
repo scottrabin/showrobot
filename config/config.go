9217aff2de65eb0542f0b691442248821d1d258e
Scott Rabin <scott@huddler-inc.com>
2014-04-28 11:46:12 -0700

    Enable loading configuration from an external file
    Also, go-fmt. Really needed a git hook...

--- config/config.go
+++ config/config.go
@@ -1,55 +1,84 @@
 package config
 
+import "encoding/json"
 import "fmt"
+import "io/ioutil"
+import "os"
+import "os/user"
+import "path/filepath"
 
 type Configuration struct {
-  Format string
-  Language string
+	Format   string `json:"format"`
+	Language string `json:"language"`
 }
 
 type InvalidPropertyAccess struct {
-  Property string
+	Property string
 }
 
 func (err *InvalidPropertyAccess) Error() string {
-  return fmt.Sprintf("Invalid configuration property: `%s`", err.Property)
+	return fmt.Sprintf("Invalid configuration property: `%s`", err.Property)
 }
 
-func Load() (conf Configuration) {
-  // TODO load from base file and from user settings
-  conf.Format = "{n}"
-  conf.Language = "en"
+func GetDefaultConfigurationPath() string {
+	usr, err := user.Current()
+	if err != nil {
+		return ""
+	}
 
-  return
+	return filepath.Join(usr.HomeDir, ".showrobot/config.json")
+}
+
+func Load(file string) (Configuration, error) {
+	conf := Configuration{
+		Format:   "",
+		Language: "en",
+	}
+
+	contents, err := ioutil.ReadFile(file)
+	if err == nil {
+		json.Unmarshal(contents, &conf)
+	}
+
+	return conf, err
+}
+
+func (conf *Configuration) Save(file string) error {
+	bytes, err := json.Marshal(conf)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(filepath.Dir(file), os.ModePerm)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(file, bytes, os.ModePerm)
+
+	return err
 }
 
 func (conf *Configuration) Get(property string) (value string, err error) {
-  switch property {
-  case "format":
-    value = conf.Format
-  case "language":
-    value = conf.Language
-  default:
-    err = &InvalidPropertyAccess{property}
-  }
+	switch property {
+	case "format":
+		value = conf.Format
+	case "language":
+		value = conf.Language
+	default:
+		err = &InvalidPropertyAccess{property}
+	}
 
-  return
+	return
 }
 
 func (conf *Configuration) Set(property string, value string) (err error) {
-  switch property {
-  case "format":
-    conf.Format = value
-  case "language":
-    conf.Language = value
-  default:
-    err = &InvalidPropertyAccess{property}
-  }
+	switch property {
+	case "format":
+		conf.Format = value
+	case "language":
+		conf.Language = value
+	default:
+		err = &InvalidPropertyAccess{property}
+	}
 
-  return
-}
-
-func (conf *Configuration) Save() (err error) {
-  // TODO
-  return
+	return
 }
--- showrobot.go
+++ showrobot.go
@@ -6,47 +6,49 @@ import "github.com/scottrabin/showrobot/config"
 import "os"
 
 func main() {
-  conf := config.Load()
-
-  app := cli.NewApp();
-  app.Name = "showrobot"
-  app.Version = "0.0.1"
-  app.Usage = "Classify & rename media files using remote datasources"
-  app.Commands = []cli.Command{
-    {
-      Name: "config",
-      Usage: "get or set the configuration value for a given key",
-      Description: `Modify the default configuration values for showrobot
+	app := cli.NewApp()
+	app.Name = "showrobot"
+	app.Version = "0.0.1"
+	app.Usage = "Classify & rename media files using remote datasources"
+	app.Commands = []cli.Command{
+		{
+			Name:  "config",
+			Usage: "get or set the configuration value for a given key",
+			Description: `Modify the default configuration values for showrobot
 when run without command line overrides`,
-      Action: func(c *cli.Context) {
-        var err error
-        var value string
-        args := c.Args()
+			Flags: []cli.Flag{
+				cli.StringFlag{"file", config.GetDefaultConfigurationPath(), "Use the specified configuration file"},
+			},
+			Action: func(c *cli.Context) {
+				var err error
+				var value string
+				args := c.Args()
+				conf, _ := config.Load(c.String("file"))
 
-        switch len(args) {
-        case 2:
-          // set the configuration value
-          err = conf.Set(args[0], args[1])
-          if err == nil {
-            conf.Save()
-          }
-        case 1:
-          // get the configuration value
-          value, err = conf.Get(args[0])
-          if err == nil {
-            fmt.Println(value)
-          }
-        default:
-          err = fmt.Errorf(
-            "showrobot config requires 1 or 2 arguments; got %d", len(args))
-        }
+				switch len(args) {
+				case 2:
+					// set the configuration value
+					err = conf.Set(args[0], args[1])
+					if err == nil {
+						err = conf.Save(c.String("file"))
+					}
+				case 1:
+					// get the configuration value
+					value, err = conf.Get(args[0])
+					if err == nil {
+						fmt.Println(value)
+					}
+				default:
+					err = fmt.Errorf(
+						"showrobot config requires 1 or 2 arguments; got %d", len(args))
+				}
 
-        if err != nil {
-          fmt.Println(err)
-          os.Exit(1)
-        }
-      },
-    },
-  }
-  app.Run(os.Args)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			},
+		},
+	}
+	app.Run(os.Args)
 }
